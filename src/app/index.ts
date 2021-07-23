e1f835130198b0952455719567baa814a0195914
juddus <github@juddus.com>
2021-07-23 14:42:14 +0100

    v.1.0.5

--- src/app/index.ts
+++ src/app/index.ts
@@ -35,7 +35,7 @@ customElements.define('ace-app', AceElement);
 
 // Entry point for Ace.
 class AceController {
-  public version = '1.0.4';
+  public version = '1.0.5';
 
   // Element in page that activates Ace.
   public buttonElement: Element | undefined;
